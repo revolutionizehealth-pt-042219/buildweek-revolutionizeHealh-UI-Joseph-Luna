093bcb154340aa55330388f854294ee943976aa8
Joseph Luna <josephluna@Josephs-MBP.local>
2019-05-01 23:10:17 -0500

    commiting filter for member and cards, using filter for completed goals

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,35 @@
 //Mobile Nav
 
-const menuSlider = document.querySelector('.menu-button')
-const nav = document.querySelector('.nav-wrapper')
+const menuSlider = document.querySelector('.menu-button');
+const nav = document.querySelector('.nav-wrapper');
+let state = false;
 
-menuSlider.addEventListener('click', event => {;
+menuSlider.addEventListener('click', event => {
 
-      TweenMax.from(".nav-wrapper",1,{opacity:0, y:-200, ease:Bounce.easeOut});
-      nav.classList.toggle('nav-wrapper');
-      nav.classList.toggle('mobile-menu')
+    TweenMax.from(".nav-wrapper", 1, { opacity: 0, y: -200, ease: Bounce.easeOut });
+
+    nav.classList.toggle('nav-wrapper');
+    nav.classList.toggle('mobile-menu')
 
 
-  });
-
-  window.addEventListener('resize', event => {
-         if (window.screen.width > 500){
-          nav.classList.add('nav-wrapper');
-          nav.classList.remove('mobile-menu');
-         }
 });
+
+window.addEventListener('resize', event => {
+
+    if (window.innerWidth === 501) {
+        nav.classList.add('nav-wrapper');
+        nav.classList.remove('mobile-menu');
+        location.reload()
+    }
+
+
+});
+
+
+function refreshDiv(divId) {
+    var curDivContent = document.getElementsByClassName(divId).innerHTML;
+    console.log(document.getElementsByClassName(divId).innerHTML)
+    document.getElementsByClassName(divId).innerHTML = curDivContent;
+    console.log('execute')
+
+}
